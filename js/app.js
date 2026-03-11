bb3268038920d82f7dcfb0ba7b6786234b5d684b
Tedarick-G <gular.k1968@gmail.com>
2026-03-11 22:06:46 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ import {
   depotFromNoisyPaste,
 } from "./utils.js";
 
-const API_BASE = "https://doretest.tvkapora.workers.dev";
+const API_BASE = "https://crawler.iremolguntr.workers.dev";
 const $ = s => document.querySelector(s);
 
 const COMPEL_HDR = [
